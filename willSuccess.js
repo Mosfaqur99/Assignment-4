4ad7d73964b74342e38465eca4e003473eb70edc
Mosfaqur Rahman <mosfaqur99@gmail.com>
2025-02-18 17:03:54 +0600

    will Success done.

--- willSuccess.js
+++ willSuccess.js
@@ -1,1 +1,26 @@
-function willSuccess(marks) {}
+function willSuccess(marks) {
+
+    let pass = 0;
+    let fail = 0;
+
+    for(const mark of marks){
+        if(mark>=50){
+            pass = pass + 1;
+        }
+        else{
+            fail = fail + 1;
+        }
+    }
+
+    if(pass > fail){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+
+}
+
+const gotChanced = willSuccess([50,46,100,80,74,12,44,45,10,45,78,95,33,49,70])
+console.log(gotChanced)
